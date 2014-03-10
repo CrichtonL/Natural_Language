cc25f97b46b205ec52cbea8294d4ef975f508b90
NerdonblooR <njbbtan@gmail.com>
2014-03-10 17:33:45 -0400

    something

--- A2/generateAM.m
+++ A2/generateAM.m
@@ -1,4 +1,3 @@
-align_ibm1('./Training', 1000, 40, 'AM_1000_40.mat');
 align_ibm1('./Training', 10000, 40, 'AM_10000_40.mat');
 align_ibm1('./Training', 15000, 40, 'AM_15000_40.mat');
 align_ibm1('./Training', 30000, 40, 'AM_30000_40.mat');
