888276ea31ae937447709377864c39a45838f2eb
NerdonblooR <njbbtan@gmail.com>
2014-03-10 14:25:41 -0400

    fix align

--- A2/evalAlign.m
+++ A2/evalAlign.m
@@ -35,7 +35,7 @@ for i=1:length(f_lines)
 end
 
 
-for fn=1:length(fn_AM)
+for fn=1:4
     CUR_AM = fn_AM{fn}
     AMFE = load(CUR_AM);
     AMFE = AMFE.AM;
@@ -54,25 +54,33 @@ for fn=1:length(fn_AM)
     num_correct_word = 0;
     num_correct_pos_and_word = 0;
 
-    for i=1:length(eng)
+    for i=1:length(f_lines)
         correct_answer = strsplit(' ', eng{i});
         translate_result = translated_eng{i};
+        %trim sentstart and sentend
+        correct_answer = correct_answer(2:length(correct_answer)-1);
+        translate_result = translate_result(2:length(correct_answer)-1);
         % if the position of the translated word is within def from the position it should to be
         % this translated word is considered as in the correct position.
         def = abs(length(correct_answer) - length(translate_result));
+        len = min(length(correct_answer),length(translate_result));
 
-        for j=1:length(correct_answer)
-            pos = find(strcmp(translate_result, correct_answer{j}));
-            % check to see if correct answer has the translated word
-            if ~isempty(pos)
+
+        for j=1:length(f_lines)
+            if ~isempty(find(ismember(translate_result, correct_answer{j})))
                 num_correct_word = num_correct_word + 1;
-                % check to see if the ranslated world is at a resonable position
-                if abs(pos - j) <= def
-                    num_correct_pos_and_word = num_correct_pos_and_word + 1;
-                end
-            end        
+            end    
+            % check to see if the ranslated world is at a resonable position
+            start_pos = max(1,j-def);
+            end_pos = min(j+def,len);
+            result_part = translate_result(start_pos:end_pos)
+            pos = find(strcmp(result_part, correct_answer{j}));
+            if ~isempty(pos)
+                num_correct_pos_and_word = num_correct_pos_and_word + 1;       
+            end
+                   
         end
-        total_word_num = total_word_num + length(correct_answer);
+        total_word_num = total_word_num + len;
     end
     r_correct_words = num_correct_word / total_word_num;
     r_correct_pos = num_correct_pos_and_word / total_word_num;
